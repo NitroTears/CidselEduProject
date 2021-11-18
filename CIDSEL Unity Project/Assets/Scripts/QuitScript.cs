e75064c261935ab4a716654444ae8c50762caf78
Finch360 <rjf013@student.usc.edu.au>
2021-11-19 09:00:07 +1000

    Adding quit functionality at end of game

---
+++ CIDSEL Unity Project/Assets/Scripts/QuitScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour
+{
+    // Quit the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
